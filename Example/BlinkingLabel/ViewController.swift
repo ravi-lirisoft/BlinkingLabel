240913ec854745a70ae9c89a4cc28c6a26d0d7b5
Ravi Prajapat <ravi.prajapat@lirisoft.com>
2022-02-24 11:47:31 +0530

    Initial commit

---
+++ Example/BlinkingLabel/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BlinkingLabel
+//
+//  Created by Ravi Prajapat on 02/24/2022.
+//  Copyright (c) 2022 Ravi Prajapat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
